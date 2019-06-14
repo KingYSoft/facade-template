0149ed9dcbd0135ab28ad90e1f33f4db300229bf
xiaohua <ytoforever@aliyun.com>
2019-06-14 14:28:39 +0800

    优化

--- test/FacadeCompanyName.FacadeProjectName.Tests/FacadeProjectNameTestBase.cs
+++ test/FacadeCompanyName.FacadeProjectName.Tests/FacadeProjectNameTestBase.cs
@@ -5,7 +5,9 @@ namespace FacadeCompanyName.FacadeProjectName.Tests
     public abstract class FacadeProjectNameTestBase : AbpIntegratedTestBase<FacadeProjectNameTestModule>
     {
         protected FacadeProjectNameTestBase()
-        { 
+        {
+            AbpSession.TenantId = 1;
+            AbpSession.UserId = 10000;
         }
     }
 }
