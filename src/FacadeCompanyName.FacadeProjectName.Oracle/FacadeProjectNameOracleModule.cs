37f65c7084aa7102b4af58873a56a011d2f00091
xiaohua <ytoforever@hotmail.com>
2024-12-02 18:21:27 +0800

    .

--- src/FacadeCompanyName.FacadeProjectName.Oracle/FacadeProjectNameOracleModule.cs
+++ src/FacadeCompanyName.FacadeProjectName.Oracle/FacadeProjectNameOracleModule.cs
@@ -1,6 +1,8 @@
-﻿using Abp.Modules;
+﻿using Abp.EntityFrameworkCore.Configuration;
+using Abp.Modules;
 using Abp.Reflection.Extensions;
 using FacadeCompanyName.FacadeProjectName.DomainService.Share;
+using FacadeCompanyName.FacadeProjectName.Oracle.EntityFrameworkCore;
 
 namespace FacadeCompanyName.FacadeProjectName.Oracle
 {
@@ -15,6 +17,17 @@ namespace FacadeCompanyName.FacadeProjectName.Oracle
         }
         public override void PreInitialize()
         {
+            Configuration.Modules.AbpEfCore().AddDbContext<FacadeProjectNameOracleDbContext>(options =>
+            {
+                if (options.ExistingConnection != null)
+                {
+                    FacadeProjectNameOracleDbContextConfigurer.Configure(options.DbContextOptions, options.ExistingConnection);
+                }
+                else
+                {
+                    FacadeProjectNameOracleDbContextConfigurer.Configure(options.DbContextOptions, options.ConnectionString);
+                }
+            });
         }
 
         public override void Initialize()
