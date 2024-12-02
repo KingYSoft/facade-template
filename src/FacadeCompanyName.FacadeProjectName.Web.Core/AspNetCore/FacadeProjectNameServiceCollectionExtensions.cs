2cf2a538b5d6c4ca41efafbc4daf414673d65f53
xiaohua <ytoforever@hotmail.com>
2024-12-02 16:23:39 +0800

    remove demo

--- src/FacadeCompanyName.FacadeProjectName.Web.Core/AspNetCore/FacadeProjectNameServiceCollectionExtensions.cs
+++ src/FacadeCompanyName.FacadeProjectName.Web.Core/AspNetCore/FacadeProjectNameServiceCollectionExtensions.cs
@@ -88,7 +88,7 @@ namespace FacadeCompanyName.FacadeProjectName.Web.Core.AspNetCore
             app.UseAbpRequestLocalization();
 
             // demo middleware
-            app.UseMiddleware<DemoMiddleware>();
+            // app.UseMiddleware<DemoMiddleware>();
         }
     }
 }
